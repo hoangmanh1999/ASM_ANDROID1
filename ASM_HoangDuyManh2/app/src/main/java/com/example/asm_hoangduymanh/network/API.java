cc9f22c3f88d5b47a0b3f5863c1670112afc8aa7
hoangmanh1999 <54472253+hoangmanh1999@users.noreply.github.com>
2021-03-11 00:06:46 +0700

    update

--- ASM_HoangDuyManh2/app/src/main/java/com/example/asm_hoangduymanh/network/API.java
+++ ASM_HoangDuyManh2/app/src/main/java/com/example/asm_hoangduymanh/network/API.java
@@ -8,9 +8,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface API {
-    String APIKEY ="b8GfmQLW9yEl67hsXriNBX9HBBHsYGlM";
+    //String APIKEY ="b8GfmQLW9yEl67hsXriNBX9HBBHsYGlM";
     String SERVER ="http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
-    String URI = "353412?apikey="+APIKEY+"&language=vi-vn&metric=true";
+    String URI = "353412?apikey=b8GfmQLW9yEl67hsXriNBX9HBBHsYGlM&language=vi-vn&metric=true";
     @GET(URI)
     Call<List<Item>> GetListData();
 }
